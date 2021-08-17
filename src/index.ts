4415a3a903c3583f07e1aaa536dec63d9b42a29d
happou31 <happo31@gmail.com>
2021-08-17 22:31:40 +0900

    feat: add refresh token 使って access token 再取得用API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import fetch from "node-fetch";
 import env from "./config/env";
+import cors from "cors";
+
 import { error, success } from "./utils/responseTool";
 
 const scope = encodeURIComponent(
@@ -15,6 +17,11 @@ const redirectUri = encodeURIComponent(
 );
 const clientId = env.clientId;
 const secret = env.clientSecret;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "*"
+}));
 
 app.post("/authorize", (req, res) => {
   const oauth2Url = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code&access_type=offline&approval_prompt=force`;
@@ -50,6 +57,25 @@ app.get("/redirect", async (req, res) => {
     </body>
   </html>`
   );
+});
+
+app.post("/token", async (req, res) => {
+  const refreshToken = req.body["refresh_token"];
+  if (!refreshToken) {
+    error(res, "refresh_token is required.", 400);
+    return;
+  }
+  const response = await fetch("https://www.googleapis.com/oauth2/v4/token", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: `client_id=${clientId}&client_secret=${secret}&refresh_token=${refreshToken}&grant_type=refresh_token`,
+  });
+
+
+  const data = await response.json();
+  success(res, data);
 });
 
 app.listen(env.port);
